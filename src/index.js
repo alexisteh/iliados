7ac4161ebd3867af62868a489e78c0e327b2df57
alexisteh <wenhui.teh23@gmail.com>
2020-07-21 01:08:02 -0400

    working text display

--- src/index.js
+++ src/index.js
@@ -89,24 +89,30 @@ document.addEventListener('DOMContentLoaded', function(){
     // <-- end of logging in and signing up 
 
 
-    // display text in container functionality 
+    // display text in container functionality --> 
 
     const textContainer = qs('div#text-container') 
     const textTable = qs('table#text-table')
-    const currentChunkNumber = "1.1"  
-    function loadText(chunkNumber){
-        let book = parseInt(chunkNumber.split(".")[0]) 
-        let startPoint = parseInt(chunkNumber.split(".")[1]) 
-        let endPoint = startPoint + 49 
-        for (let i = startPoint; i < endPoint; i ++ ){
-            const currentReq = new XMLHttpRequest()
-            console.log("https://www.perseus.tufts.edu/hopper/CTS?request=GetPassage&urn=urn:cts:greekLit:tlg0012.tlg001:"+book+"."+i)
-            currentReq.open("GET", "https://www.perseus.tufts.edu/hopper/CTS?request=GetPassage&urn=urn:cts:greekLit:tlg0012.tlg001:"+book+"."+i, true)
-            currentReq.onload = function() {
+    
+    // load next 50 lines after chunkNumber, inclusive
+    function loadText(chunkNumber){       
+        textTable.innerHTML ="" 
+        const book = parseInt(chunkNumber.split(".")[0]) 
+        const startPoint = parseInt(chunkNumber.split(".")[1]) 
+        const endPoint = startPoint + 49 
+        grabLine(book, startPoint, endPoint, 0) 
+    }
+
+    function grabLine(book, i, endI, skip){ 
+        const currentReq = new XMLHttpRequest()
+        console.log("https://www.perseus.tufts.edu/hopper/CTS?request=GetPassage&urn=urn:cts:greekLit:tlg0012.tlg001:"+book+"."+i)
+        currentReq.open("GET", "https://www.perseus.tufts.edu/hopper/CTS?request=GetPassage&urn=urn:cts:greekLit:tlg0012.tlg001:"+book+"."+i, true)
+        currentReq.onload = function() { 
+            if (currentReq.responseText.split('Invalid URN reference').length == 1){
                 let lineOutput = currentReq.responseText.split('<tei:div type="line">')[1].split("</tei:div>")[0]
                 if (lineOutput.split('<milestone ed="P" unit="para"/>').length == 2){
                     lineOutput = "<br/>" + lineOutput.split('<milestone ed="P" unit="para"/>')[1]
-                }
+                } 
                 console.log(lineOutput)
                 console.log(lineOutput) 
                 const currentTableRow = ce('tr')
@@ -115,14 +121,66 @@ document.addEventListener('DOMContentLoaded', function(){
                 const currentLineText = ce('td')
                 currentLineText.innerHTML= lineOutput 
                 currentTableRow.append(currentLineNumber, currentLineText)
-                textTable.append(currentTableRow)
+                textTable.append(currentTableRow) 
+                if (i + 1 <= endI){
+                    grabLine(book, i+1, endI, 0)  
+                } else {
+                    console.log("end")
+                } 
             } 
-            currentReq.send() 
-
+            else { 
+                let passes = skip + 1
+                let interval = 1 
+                let delayedLine = "" 
+                while (passes > 0 ){ 
+                    if (currentReq.responseText.split(`<cite n="${i + interval}">`).length == 2) {
+                        const chunkAfter = currentReq.responseText.split(`<cite n="${i + interval}">`)[1]
+                        delayedLine = chunkAfter.split('</cite>')[0] 
+                        console.log(delayedLine) 
+                        passes = passes - 1
+                    }  
+                    interval = interval + 1 
+                    console.log(interval) 
+                    if (interval >= 20){
+                        const currentTableRow = ce('tr')
+                        const currentLineNumber = ce('td') 
+                        const currentLineText = ce('td')
+                        currentLineText.innerHTML= "END"   
+                        currentLineText.style.textAlign="center"
+                        currentLineText.style.fontWeight="bold"
+                        currentTableRow.append(currentLineNumber, currentLineText)
+                        textTable.append(currentTableRow) 
+                        return 
+                    } 
+                } 
+                const currentTableRow = ce('tr')
+                const currentLineNumber = ce('td') 
+                currentLineNumber.innerText = i 
+                const currentLineText = ce('td')
+                currentLineText.innerHTML= delayedLine  
+                currentTableRow.append(currentLineNumber, currentLineText)
+                textTable.append(currentTableRow) 
+                if (i + 1 <= endI){ 
+                    console.log(skip) 
+                    grabLine(book, i+1, endI, skip + 1)  
+                } else { 
+                    console.log("end") 
+                }  
+            }
         } 
+        currentReq.send() 
     }
-    loadText(currentChunkNumber) 
 
+    const manualNavNumber = qs('form#go-to-page')
 
-
+    manualNavNumber.addEventListener('submit', function(){
+        event.preventDefault()
+        const input = qs('#smallnuminput').value 
+        const inputLine = parseInt(input.split(".")[1])
+        const closestPoint = inputLine - (inputLine % 50) +1 
+        const closestOutput = `${input.split(".")[0]}.${closestPoint}`
+        const blank = qs('input#smallnuminput')
+        blank.value = closestOutput 
+        loadText(closestOutput) 
+    }) 
 })
